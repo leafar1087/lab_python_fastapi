2539853f9d90677bcbf1967861f5be34bc817f95
Rafael Perez <leafar1087@gmail.com>
2024-08-28 21:46:52 -0500

    actualizacion archivos test1 y test2

---
+++ sesion_02/test_01.py
@@ -0,0 +1,13 @@
+"""TIPOS DE DATOS"""
+
+"""NUMERICOS"""
+
+var1 = 40
+var2 = 105
+var3 = 60.3
+var4 = -10.56
+
+print("El valor de la variable 1 es: ", var1)
+print("El valor de la variable 2 es: ", var2)
+print("El valor de la variable 3 es: {}".format(var3))
+print("El valor de la variable 4 es: {}".format(var4))
---
+++ sesion_02/test_02.py
@@ -0,0 +1,19 @@
+"""TIPOS DE DATOS"""
+
+"""BOOL"""
+
+var1 = True
+var2 = False
+var3 = []
+var4 = 0
+
+print("El valor de la variable 1 es: {}".format(var1))
+print("El valor de la variable 2 es: {}".format(var2))
+print("El valor de la variable 3 es: {}".format(var3))
+print("El valor de la variable 4 es: {}".format(var4))
+
+if var1:
+    print("Hi, Python")
+
+if var2:
+    print("Hola, Python")
